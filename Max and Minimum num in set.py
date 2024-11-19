d45e4083eb2b6bdcafade66283a26dcac732a1ed
Abeera-saqib <abeerasaqib84@gmail.com>
2024-11-19 15:07:03 +0000

    Add files via upload

---
+++ Max and Minimum num in set.py
@@ -0,0 +1,8 @@
+setn = {30, 20, 100, 40, 35, 19, 87, 31}
+print("Original set elements: ")
+print(setn)
+print(type(setn))
+print("\nMaximum value of the said set: ")
+print(max(setn))
+print("\nMinimum value of the said set: ")
+print(min(setn))
