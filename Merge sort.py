9e8811e1dcc56d876c7be3492ec58e6d214c016e
Abeera-saqib <abeerasaqib84@gmail.com>
2024-11-19 15:07:13 +0000

    Add files via upload

---
+++ Merge sort.py
@@ -0,0 +1,50 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    print("Merging:", arr)
+
+ 
+
+# Get user input for numbers
+
+input_str = input("Enter a list of numbers separated by spaces: ")
+
+numbers = [int(num) for num in input_str.split()]
+
+ 
+
+# Call merge_sort function
+
+merge_sort(numbers)
+
+ 
+
+# Print the final sorted numbers
+
+print("Final Sorted numbers:", numbers)
