361ecd79410942ddf22ec478fe8dc049b27bb775
Abeera-saqib <abeerasaqib84@gmail.com>
2024-11-19 15:07:21 +0000

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,4 @@
+original_name = input("What is your name = ")
+reverse_name = original_name[:: -1]
+ispalindrome = (original_name == reverse_name)
+print("the given name is palindrome = ", ispalindrome)
